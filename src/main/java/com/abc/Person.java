c1aca1c33ee09931bdeec398fc84d3c962f99ea2
licj <lichaojie@qq.com>
2020-10-27 14:06:08 +0800

    'init'

---
+++ src/main/java/com/abc/Person.java
@@ -0,0 +1,13 @@
+package com.abc;
+
+/**
+ * @author lcj
+ * @version 1.0
+ * @date 2020/10/26 17:43
+ */
+public class Person {
+    private String name;
+    private String age;
+    private Integer phone;
+    private Integer sex;
+}
